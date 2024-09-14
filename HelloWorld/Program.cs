f63a9d73dd4298951bca4a532ca5dff28b41e2d5
Marco Osorio <marco.osorio86@hotmail.com>
2024-09-14 15:06:36 -0500

    Added some basic C# code

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Las constantes no permiten ser reasignadas.
+            const string Name = "Marco";
+            // Interpolación de cadenas, sirve para dar formato a las cadenas de texto (Strings), similar al 'f' de python, y con la misma estructura js
+            Console.WriteLine($"Hello {Name}!");
+            Console.ReadLine();
+        }
+    }
+}
